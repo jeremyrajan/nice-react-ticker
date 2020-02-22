3577cb99149444a76b993bf00d87c1a48eb98afd
Jeremy Rajan <jeremyrajan@gmail.com>
2020-02-22 17:00:06 +0800

    add link

--- src/tickers/NewsTicker/index.tsx
+++ src/tickers/NewsTicker/index.tsx
@@ -13,13 +13,13 @@ interface NewsTickerProps {
 
 const NewsTicker: React.FunctionComponent<NewsTickerProps> = (props) => {
   return (
-    <div className={[newsStyles.newsticker, genericStyles.tickerItem].join(' ')}>
+    <a className={[newsStyles.newsticker, genericStyles.tickerItem].join(' ')} href={props.url} target="_blank">
       <div className={newsStyles.icon}><img src={props.icon} alt={props.iconAlt} /></div>
       <div className={newsStyles.newsWrapper}>
         <div className={newsStyles.title}>{props.title}</div>
         <small className={newsStyles.meta}>{props.meta}</small>
       </div>
-    </div>
+    </a>
   )
 }
 
