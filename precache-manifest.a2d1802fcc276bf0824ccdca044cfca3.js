6bf301656c3d9c87f1c9911aad10fc577185a6f0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-12-26 14:41:46 +0000

    Deploy jeremyrajan/nice-react-ticker to github.com/jeremyrajan/nice-react-ticker.git:gh-pages

---
+++ precache-manifest.a2d1802fcc276bf0824ccdca044cfca3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f98b4a372fed58000b09",
+    "url": "/nice-react-ticker/static/css/main.7de24174.chunk.css"
+  },
+  {
+    "revision": "f98b4a372fed58000b09",
+    "url": "/nice-react-ticker/static/js/main.f98b4a37.chunk.js"
+  },
+  {
+    "revision": "2f6976e1beccdabb6331",
+    "url": "/nice-react-ticker/static/js/1.2f6976e1.chunk.js"
+  },
+  {
+    "revision": "a3336d87c2130cf55d84",
+    "url": "/nice-react-ticker/static/js/runtime~main.a3336d87.js"
+  },
+  {
+    "revision": "9b7cc3ca0d4dbf7dbd0511683f92c759",
+    "url": "/nice-react-ticker/index.html"
+  }
+];
